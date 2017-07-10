993d821b56835dad7998704a141e346029081415
lfrydrych <lfrydrych@pgs-soft.com>
2017-07-10 14:51:32 +0200

    First test for task 2

---
+++ SeleniumFrameworkCsharp/Pages/Executors/Task2Page.cs
@@ -0,0 +1,49 @@
+﻿using SeleniumFrameworkCsharp.Utilities;
+using SeleniumFrameworkCsharp.Pages.Locators;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+using SeleniumFrameworkCsharp.Utilities.Helpers;
+
+namespace SeleniumFrameworkCsharp.Pages.Executors
+{
+    class Task2Page : AbstractPage
+    {
+        public Task2PageLocators locators;
+
+        public Task2Page(SeleniumExecutor executor)
+            : base(executor)
+        {
+            InitTask2PageElements();
+        }
+
+        public void InitTask2PageElements()
+        {
+            locators = new Task2PageLocators();
+            PageFactory.InitElements(SeleniumExecutor.GetDriver(), locators);
+        }
+
+        public void ExpandDropDownList()
+        {
+            locators.dropDownArrow.Click();
+        }
+
+        public void TypeTextInSearchBox(string text)
+        {
+            locators.searchBox.SendKeysWithWait(text);
+            locators.searchBox.SendKeys(Keys.Enter);
+        }
+
+        public bool VerifyCategoriesOfSelectedProducts(string category)
+        {
+            bool areProperCategories = true;
+            var lista = locators.categoriesOfProducts;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (!lista[i].Text.Equals(category)) {
+                    areProperCategories = false;
+                }
+            }
+            return areProperCategories;
+        }
+    }
+}
---
+++ SeleniumFrameworkCsharp/Tests/Features/Task2.feature
@@ -0,0 +1,17 @@
+﻿@Sorting
+Feature: Task2
+
+Background: 
+Given I open task 2 page
+
+
+Scenario Outline: 01 User is able to search for category
+When I expand dropdown list
+And I type fragment "<fragment>" of category name
+Then I can see products only for this category "<category>"
+
+Examples: 
+| fragment | category       |
+| el       | Elektronika    |
+| ort      | Sport          |
+| a i usł  | Firma i usługi | 
