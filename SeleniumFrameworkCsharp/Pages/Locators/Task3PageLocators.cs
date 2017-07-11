9583dab0d01da03f2db6a8b1b4b06265557c0b01
lfrydrych <lfrydrych@pgs-soft.com>
2017-07-11 10:47:22 +0200

    Test for task 3

---
+++ SeleniumFrameworkCsharp/Pages/Locators/Task3PageLocators.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumFrameworkCsharp.Pages.Locators
+{
+    class Task3PageLocators
+    {
+        [FindsBy(How = How.CssSelector, Using = ".dropdown-toggle.menu-border")]
+        public IWebElement menuToggle;
+
+        [FindsBy(How = How.CssSelector, Using = "#menu1>li>a")]
+        public IWebElement subMenuForm;
+
+        [FindsBy(How = How.CssSelector, Using = "#start-edit")]
+        public IWebElement startEdit;
+
+        [FindsBy(How = How.Id, Using = "in-name")]
+        public IWebElement nameInput;
+
+        [FindsBy(How = How.Id, Using = "in-surname")]
+        public IWebElement surnameInput;
+
+        [FindsBy(How = How.Id, Using = "in-notes")]
+        public IWebElement notesInput;
+
+        [FindsBy(How = How.Id, Using = "in-phone")]
+        public IWebElement phoneInput;
+
+        [FindsBy(How = How.Id, Using = "in-file")]
+        public IWebElement fileInput;
+
+        [FindsBy(How = How.Id, Using = "save-btn")]
+        public IWebElement saveButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".notifyjs-bootstrap-base.notifyjs-bootstrap-success>span")]
+        public IWebElement confirmationMessage;
+
+        [FindsBy(How = How.CssSelector, Using = ".preview-photo")]
+        public IWebElement image;
+    }
+}
---
+++ SeleniumFrameworkCsharp/Tests/Steps/Task3Steps.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using SeleniumFrameworkCsharp.Pages.Executors;
+using SeleniumFrameworkCsharp.Utilities;
+using TechTalk.SpecFlow;
+
+namespace SeleniumFrameworkCsharp.Tests.Steps
+{
+    [Binding]
+    public class Task3Steps
+    {
+        private Task3Page task3page;
+        private SeleniumExecutor executor;
+
+        public Task3Steps()
+        {
+            executor = SeleniumExecutor.GetExecutor();
+        }
+
+        [BeforeScenario]
+        public void BeforePage()
+        {
+            task3page = new Task3Page(executor);
+        }
+
+        [When(@"I start the editable mode")]
+        public void WhenIStartTheEditableMode()
+        {
+            task3page.SetEditableMode();
+        }
+
+        [When(@"I fill all fields with correct data")]
+        public void WhenIFillAllFieldsWithCorrectData()
+        {
+            task3page.ClearAllFields();
+            task3page.FillInForm();
+        }
+
+        [When(@"I upload photo")]
+        public void WhenIUploadPhoto()
+        {
+            task3page.UploadFile();
+        }
+
+        [When(@"I click save button")]
+        public void WhenIClickSaveButton()
+        {
+            task3page.SaveForm();
+        }
+
+        [Then(@"Message ""(.*)"" should appears")]
+        public void ThenMessageShouldAppears(string message)
+        {
+            Assert.AreEqual(message, task3page.GetConfirmationMessageWithWait());
+        }
+
+        [Then(@"All fields have correct data")]
+        public void ThenAllFieldsHaveCorrectData()
+        {
+            Assert.AreEqual(task3page.Name, task3page.VerifyName());
+            Assert.AreEqual(task3page.Surname, task3page.VerifySurname());
+            Assert.AreEqual(task3page.Notes, task3page.VerifyNotes());
+            Assert.AreEqual(task3page.Phone, task3page.VerifyPhone());
+            Assert.True(task3page.VerifyPhotoSrc().Contains("data:image/jpeg;base64"));
+        }
+    }
+}
